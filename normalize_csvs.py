c4bc3c3ed310790cf538c30e78d42e07bd93bf25
dimitrisl <dlolis88@gmail.com>
2019-09-18 13:02:32 +0200

    fix multiple entries of the column title

--- normalize_csvs.py
+++ normalize_csvs.py
@@ -8,5 +8,14 @@ def get_dataframes():
     folder = "sample_data/Norwegian well  completion reports and label data for hydrocarbon shows"
     keep_csvs = { i: folder+"/"+i for i in os.listdir(folder) if ".csv" in i}
     print("found", keep_csvs.keys()) 
-    csv_dataframes = {i: pd.read_csv(keep_csvs[i], sep=';', skiprows=range(9), encoding='iso-8859-1') for i in keep_csvs.keys() }
+    # csv_dataframes = {i: pd.read_csv(keep_csvs[i], sep=';', skiprows=range(9), encoding='iso-8859-1') for i in keep_csvs.keys() }
+    
+    csv_dataframes = {}
+    for i in keep_csvs.keys():
+        csv_dataframes[i] = pd.read_csv(keep_csvs[i], sep=';', skiprows=range(9), encoding='iso-8859-1')
+        temp_clean = csv_dataframes[i]
+        col = temp_clean.columns[0]
+        csv_dataframes[i] = temp_clean[temp_clean[col] != col].copy()
+        temp_clean = None
+        
     return csv_dataframes
---
+++ rectify_dataframes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from data_load_functions import create_npd_shapefile_dict
+
+from normalize_csvs import get_dataframes
+
+## take the comments from what i've built and use a column name "csv_name"+comment
+
+
+dict_dataframes = get_dataframes()
+
+lala = create_npd_shapefile_dict()

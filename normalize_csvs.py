64a59a33ecadd718c4dfee5ac0cd62a1a6ff5b06
dimitrisl <dlolis88@gmail.com>
2019-09-18 12:10:15 +0200

    path/name fix

--- normalize_csvs.py
+++ normalize_csvs.py
@@ -4,9 +4,11 @@
 import os
 import pandas as pd
 
-def get_datafames():
-    folder = "/home/dimitrisl/Desktop/FORCE_Geolocation_Docs/sample_data/Norwegian well  completion reports and label data for hydrocarbon shows"
+def get_dataframes():
+    folder = "sample_data/Norwegian well  completion reports and label data for hydrocarbon shows"
     keep_csvs = { i: folder+"/"+i for i in os.listdir(folder) if ".csv" in i}
     print("found", keep_csvs.keys()) 
     csv_dataframes = {i: pd.read_csv(keep_csvs[i], sep=';', skiprows=range(9), encoding='iso-8859-1') for i in keep_csvs.keys() }
     return csv_dataframes
+
+get_dataframes()
